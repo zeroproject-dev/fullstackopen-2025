641d6db430b8fa117d4b8f4be860cbb831922c9d
Andres Copeticona <andrescopeticona@outlook.es>
2025-01-31 12:14:26 -0400

    part1 exercise 13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const [selected, setSelected] = useState(0);
 
@@ -19,9 +20,16 @@ const App = () => {
     setSelected(randomIndex);
   };
 
+  const handleVote = () => {
+    votes[selected] += 1;
+    setVotes([...votes]);
+  };
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes.</p>
+      <button onClick={handleVote}>Vote 👆</button>
       <button onClick={handleNextAnecdote}>next anecdote</button>
     </div>
   );
