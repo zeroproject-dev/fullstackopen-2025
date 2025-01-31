5af34eafbeda78011f7e630635e559e52a3647a3
Andres Copeticona <andrescopeticona@outlook.es>
2025-01-31 12:00:08 -0400

    part1 exercise 9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,8 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / all;
   const positive = (good / all) * 100;
 
+  if (all === 0) return <p>No feedback given</p>;
+
   return (
     <div>
       <h2>Stadistics</h2>
