f15d96305322543746e8f19995433f92a0888162
Andres Copeticona <andrescopeticona@outlook.es>
2025-02-01 12:02:02 -0400

    part2 exercise 5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = ({ name }) => {
-  return <h1>{name}</h1>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  // Anti-pattern: using index as key, so we should use id instead
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part part={part} key={part.id} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  // I already used reduce
-  const total = parts.reduce((val, curr) => val + curr.exercises, 0);
-
-  return <strong>Number of exercises {total}</strong>;
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from "./components/course";
 
 const App = () => {
   const courses = [
