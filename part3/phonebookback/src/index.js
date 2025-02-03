7a5b7b4994479b4158ae04c9a4168c4cf24c1a6e
Andres Copeticona <andrescopeticona@outlook.es>
2025-02-03 13:01:54 -0400

    part3 exercise 8

--- part3/phonebookback/src/index.js
+++ part3/phonebookback/src/index.js
@@ -3,7 +3,13 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+morgan.token("body", (req) => {
+  if (req.method === "POST") return JSON.stringify(req.body);
+  return "No Content";
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 let persons = [
   {
