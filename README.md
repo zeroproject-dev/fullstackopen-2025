80138ada9c0d71a04fd1bcddebf4b05743630f36
Andres Copeticona <andrescopeticona@outlook.es>
2025-02-05 19:24:29 -0400

    part3 exercise 10

---
+++ README.md
@@ -0,0 +1,3 @@
+# Exercises for FullStackOpen 2024
+
+Exercise 3.10 Url for PhoneBook: [Url fly.dev](https://phonebookback.fly.dev/)
--- part3/phonebookback/src/index.js
+++ part3/phonebookback/src/index.js
@@ -103,7 +103,7 @@ app.get("/api/info", (req, res) => {
   );
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
