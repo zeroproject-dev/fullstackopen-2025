44b0d76f3ef9b51776a42189f9deaec28fe9ecc3
Andres Copeticona <andrescopeticona@outlook.es>
2025-02-09 21:01:29 -0400

    part3 exercise 20

--- part3/phonebookback/src/models/person.js
+++ part3/phonebookback/src/models/person.js
@@ -21,7 +21,16 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return v.length > 8 && /^\d{2,3}-\d*$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+    required: [true, "The phone number is required"],
+  },
 });
 
 personSchema.set("toJSON", {
