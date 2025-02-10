4d665e2f45b210e0ec99c15d9a8e7728bc18a93a
Andres Copeticona <andrescopeticona@outlook.es>
2025-02-10 17:31:09 -0400

    part4 exercise 5

--- part4/blogs/src/utils/list_helper.js
+++ part4/blogs/src/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes));
+  return blogs.find((blog) => blog.likes === maxLikes);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
